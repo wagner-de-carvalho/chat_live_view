d67708be67bc0da3b8d532cb1d4616582ff6516d
wagner <wagncarv@outlook.com>
2023-06-26 16:27:35 -0300

    Displaying the Topic Name

--- lib/chat_live_view_web/live/topic_live.ex
+++ lib/chat_live_view_web/live/topic_live.ex
@@ -1,7 +1,7 @@
 defmodule ChatLiveViewWeb.TopicLive do
   use ChatLiveViewWeb, :live_view
 
-  def mount(params, _session, socket) do
-    {:ok, assign(socket, form: to_form(%{}))}
+  def mount(%{"topic_name" => topic_name} = _params, _session, socket) do
+    {:ok, assign(socket, form: to_form(%{}), topic_name: topic_name)}
   end
 end
