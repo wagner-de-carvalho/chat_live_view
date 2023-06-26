7aa702941443d1fb8f4ec7bf66008cafcc5f7c43
wagner <wagncarv@outlook.com>
2023-06-26 13:42:18 -0300

    Credo linter formatting

--- test/chat_live_view_web/controllers/error_html_test.exs
+++ test/chat_live_view_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule ChatLiveViewWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(ChatLiveViewWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(ChatLiveViewWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
