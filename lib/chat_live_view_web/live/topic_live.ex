85643b3a554167047e9f428f0c4872737166c22f
wagner <wagncarv@outlook.com>
2023-06-26 17:47:14 -0300

    Setting up the Event Handlers for Chat Messages

--- lib/chat_live_view_web/live/topic_live.ex
+++ lib/chat_live_view_web/live/topic_live.ex
@@ -3,6 +3,17 @@ defmodule ChatLiveViewWeb.TopicLive do
 
   def mount(%{"topic_name" => topic_name} = _params, _session, socket) do
     username = AnonymousNameGenerator.generate_random()
-    {:ok, assign(socket, form: to_form(%{}), topic_name: topic_name, username: username)}
+
+    {:ok,
+     assign(socket, form: to_form(%{}), topic_name: topic_name, username: username, message: "")}
+  end
+
+  def handle_event("submit_message", %{"message" => message}, socket) do
+    {:noreply, assign(socket, message: message)}
+  end
+
+  def handle_event("message_change", %{"message" => message}, socket) do
+    valid_message = String.trim(message)
+    {:noreply, assign(socket, message: valid_message)}
   end
 end
