6e294847e59ce6a43ec660e658402d1a965aec39
wagner <wagncarv@outlook.com>
2023-06-27 13:24:49 -0300

    Creating Helper Functions in Phoenix LiveView (for our messages)

--- lib/chat_live_view_web/live/topic_live.ex
+++ lib/chat_live_view_web/live/topic_live.ex
@@ -25,7 +25,27 @@ defmodule ChatLiveViewWeb.TopicLive do
     {:noreply, assign(socket, message: valid_message)}
   end
 
-  def handle_info(%{event: "new_message", payload: message_data}, socket) do
+  def handle_info(%{event: "new_message", payload: _message_data}, socket) do
     {:noreply, socket}
+  end
+
+  def user_msg_heex(assigns) do
+    ~H"""
+    <li class="relative bg-white py-5 px-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 hover:bg-gray-50">
+      <div class="flex justify-between space-x-3">
+        <div class="min-w-0 flex-1">
+          <a href="#" class="block focus:outline-none">
+            <span class="absolute inset-0" aria-hidden="true"></span>
+            <p class="truncate text-sm font-medium text-gray-900 mb-4">Gloria Roberston</p>
+          </a>
+        </div>
+      </div>
+      <div class="mt-1">
+        <p class="text-sm text-gray-600 line-clamp-2">
+          Doloremque dolorem maiores assumenda dolorem facilis. Velit vel in a rerum natus facere. Enim rerum eaque qui facilis. Numquam laudantium sed id dolores omnis in. Eos reiciendis deserunt maiores et accusamus quod dolor.
+        </p>
+      </div>
+    </li>
+    """
   end
 end
